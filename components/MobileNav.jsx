aa86be50af524dd6770363a85b7947dcbfa4d618
Blendi Ivanja <blendiivanja2@gmail.com>
2024-06-17 15:41:23 +0200

    feat: add background colors, mobile navigation, and desktop header

---
+++ components/MobileNav.jsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
+
+const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger
+        className="flex justify-center items-center"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/" onClick={handleLinkClick}>
+            <h1 className="text-4xl font-semibold">
+              Blendi<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+
+        <nav className="flex flex-col items-center justify-start gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } capitalize  font-medium hover:text-accent transition-all`}
+                onClick={handleLinkClick}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
